dfa9b83912a80cf9d7177a82ab61129d688a9bfe
Mishra Gaurav <gaurav.mishra.cx@gmail.com>
2024-10-11 00:47:10 +0530

    have a default python version

--- .github/actions/setup-python-poetry/scripts/install_poetry.sh
+++ .github/actions/setup-python-poetry/scripts/install_poetry.sh
@@ -3,13 +3,16 @@
 # Exit script on error
 set -e
 
+# Define a default Python version if none is provided
+PYTHON_VERSION=${1:-"3.10"}
+
 # Update system packages
 echo "Updating system packages..."
 sudo apt-get update
 
 # Install the specified Python version
-echo "Installing Python version ${1}..."
-sudo apt-get install python${1}-dev python3-pip -y
+echo "Installing Python version $PYTHON_VERSION..."
+sudo apt-get install python"${PYTHON_VERSION}"-dev python3-pip -y
 
 # Install Poetry
 echo "Installing Poetry..."
