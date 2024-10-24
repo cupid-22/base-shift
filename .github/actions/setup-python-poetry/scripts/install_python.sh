4d898178b3e237f34ce2d70e9d7be09f4572cd66
Mishra Gaurav <gaurav.mishra.cx@gmail.com>
2024-10-25 01:17:23 +0530

    update before installing

--- .github/actions/setup-python-poetry/scripts/install_python.sh
+++ .github/actions/setup-python-poetry/scripts/install_python.sh
@@ -9,7 +9,7 @@ IFS=',' read -r -a PYTHON_VERSIONS <<< "${1:-"3.10,3.12,3.11"}"
 # Iterate through each Python version and attempt to install it
 for PYTHON_VERSION in "${PYTHON_VERSIONS[@]}"; do
   echo "Installing Python version $PYTHON_VERSION..."
-
+    sudo apt-get update
     # Check if the version is already installed
     if command -v python"$PYTHON_VERSION" &> /dev/null; then
         echo "Python $PYTHON_VERSION is already installed. Skipping..."
