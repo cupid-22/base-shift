e77590bff3ed340c4d4c02790e9892d1c7c858e6
Mishra Gaurav <gaurav.mishra.cx@gmail.com>
2024-10-25 00:33:21 +0530

    upgrading the installation

---
+++ .github/actions/setup-python-poetry/scripts/install_python.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Exit script on error
+set -e
+
+# Split the input into an array of Python versions (comma-separated)
+IFS=',' read -r -a PYTHON_VERSIONS <<< "${1:-"3.10,3.12,3.11"}"
+
+# Iterate through each Python version and attempt to install it
+for PYTHON_VERSION in "${PYTHON_VERSIONS[@]}"; do
+  echo "Installing Python version $PYTHON_VERSION..."
+
+    # Check if the version is already installed
+    if command -v python"$PYTHON_VERSION" &> /dev/null; then
+        echo "Python $PYTHON_VERSION is already installed. Skipping..."
+    else
+        sudo apt-get install -y python"${PYTHON_VERSION}"-dev python3-pip
+        echo "Python $PYTHON_VERSION installed successfully."
+    fi
+
+    # Set the installed version as default
+    sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python"${PYTHON_VERSION}" 1
+done
+
+# Check installed versions
+echo "Installed Python versions:"
+python3 --version
